46fc1e1ea10d0564d56af3f6aec87eeb6fe44a7c
Dendashka <d-dashkevich@mail.ru>
2020-09-14 13:36:45 +0300

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+  console.log(
+    "%c > Development: https://awagro.by/",
+    "background: #fc342a; padding:7px 7px 7px 0; font-size: 12px; color: #ffffff"
+  );
+  // ************************* //
+
+
+});
